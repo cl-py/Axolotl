01c5a928e255eded3af055e9fbe1f4772d8fe16d
cl-py <ivypool59@gmail.com>
2026-05-22 15:07:10 -0500

    feat: handles ipv4 packets

--- src/bpf/filtering.bpf.c
+++ src/bpf/filtering.bpf.c
@@ -9,11 +9,20 @@ int tc_ingress(struct __sk_buff *ctx)
 {
 	void *data = (void*)(long)ctx->data;
 	void *data_end = (void*)(long)ctx->data_end;
+
+	struct ethhdr *eth;
+	struct iphdr *iph;
 	
-	// IPv4 packet
 	if (ctx->protocol == bpf_htons(ETH_P_IP)){
-		// IPv4 Packet
-		return TC_ACT_OK;
+		eth = data;
+		if (void*)(eth + 1) > data_end
+			return TC_ACT_OK;
+		iph = (struct iphdr*)(eth + 1);
+
+		if (void*)(iph + 1) > data_end
+			return TC_ACT_OK;
+		bpf_printk("got IPv4 packet from: %d, incoming to %d\n", iph->saddr, iph->daddr);
+
 	}else if (ctx->protocol == bpf_htons(ETH_P_IPV6){
 		// IPv6 Packet
 		return TC_ACT_OK;
@@ -22,8 +31,6 @@ int tc_ingress(struct __sk_buff *ctx)
 		return TC_ACT_OK;
 	}
 
-	// src ip
-	struct ethhdr *6;
 
 	return TC_ACT_OK;
 }
