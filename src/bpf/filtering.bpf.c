a453d2c403f8356f9d56fa5ad38a53a1663d0048
cl-py <ivypool59@gmail.com>
2026-05-22 14:28:06 -0500

    feat: protocol parsing skeleton

--- src/bpf/filtering.bpf.c
+++ src/bpf/filtering.bpf.c
@@ -10,6 +10,18 @@ int tc_ingress(struct __sk_buff *ctx)
 	void *data = (void*)(long)ctx->data;
 	void *data_end = (void*)(long)ctx->data_end;
 	
+	// IPv4 packet
+	if (ctx->protocol == bpf_htons(ETH_P_IP)){
+		// IPv4 Packet
+		return TC_ACT_OK;
+	}else if (ctx->protocol == bpf_htons(ETH_P_IPV6){
+		// IPv6 Packet
+		return TC_ACT_OK;
+	}else{
+		// All other traffic
+		return TC_ACT_OK;
+	}
+
 	// src ip
 	struct ethhdr *6;
 
