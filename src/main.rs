fa9eeeb3443acbed03217f39a024eee7fcd737b1
Eddie <ed.murillo717@gmail.com>
2026-05-22 19:18:14 -0500

    test: adding filtering with rust library

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 // use std::library::function
 use std::mem::MaybeUninit; //allows for safe memory allocation without initialization
 use std::time::Duration; //Imports time types (seconds, etc)
+use std::os::unix::io::AsFd as  _;
 
 //these are for command-line handling
 use clap::{ArgAction, Parser};
@@ -21,6 +22,8 @@ use libbpf_rs::TC_H_CLSACT;
 use libbpf_rs::TC_H_MIN_INGRESS;
 use libbpf_rs::TC_INGRESS;
 
+// use nix::net::if_::if_nametoindex;
+
 //these are used for logging
 // use tracing::subscriber::set_global_default as set_global_subscriber;
 // use tracing_subscriber::filter::LevelFilter; //used for logging verosity
@@ -29,12 +32,12 @@ use libbpf_rs::TC_INGRESS;
 // use tracing_subscriber::FmtSubscriber; //ties all the above features together
 
 //how to import the eBPF program
-mod logging{
-    include!(concat!(env!("OUT_DIR"), "/logging.skel.rs"));
+mod filtering{
+    include!(concat!(env!("OUT_DIR"), "/filtering.skel.rs"));
 }
 mod event;
 
-use logging::*;
+use filtering::*;
 
 #[derive(Parser)]
 struct Args{
@@ -61,7 +64,7 @@ fn main() -> Result<(), libbpf_rs::Error>{
     let args = Args::parse();
 
     //Initialize BPF Skeleton -- (eBPFname)SkelBuilder
-    let skeleton_builder = LoggingSkelBuilder::default();
+    let skeleton_builder = FilteringSkelBuilder::default();
     let mut open_object = MaybeUninit::uninit();
     let open_skeleton = skeleton_builder.open(&mut open_object).unwrap(); 
     let mut skeleton = open_skeleton.load().unwrap();
@@ -69,12 +72,13 @@ fn main() -> Result<(), libbpf_rs::Error>{
     //assigns attack to something so its not destructed after it executes..was causing a silent crash
     let mut __link_state = skeleton.attach();
 
-    let mut tc_builder = TcHookBuilder::new(skel.progs.tc_ingress.as_fd());
-    tc_builder.ifindex(if_nametoindex("wlo1").unwrap()).handle(1).priority(1);
+    let mut tc_builder = TcHookBuilder::new(skeleton.progs.tc_ingress.as_fd());
+    tc_builder.ifindex(2).handle(1).priority(1);
+    //if_nametoindex("wlo1").unwrap()
 
     let mut ingress = tc_builder.hook(TC_INGRESS);
-    ingress.create().context("Failed to create TC ingress hook layer")?;
-    ingress.attach().context("Failed to attach eBPF program to ingress")?;
+    ingress.create()?;
+    ingress.attach()?;
     //creates new user ring buffer
     // let mut user_ring = libbpf_rs::UserRingBuffer::new(&skeleton.maps.user_ring).unwrap();
     // pass_arguments(user_ring);
